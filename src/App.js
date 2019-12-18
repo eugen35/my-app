513fa083c3232bceaf23a718d54de89323c15c64
_meugen_ <eugen35@mail.ru>
2019-12-18 18:10:58 +0500

    use timer не получается

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Component1 from './Component1';
 
+// function startTimer(){let t=new Date().getTime(); return setInterval(()=>console.log(Math.floor((new Date().getTime()-t)/1000)),1000)} - это с замыканием
+// p=startTimer();
+// After some time to stop Timing use:  clearInterval(p);
 
 function App() {
+  const [myState, setMyState] = useState({startUnixTime: undefined, currentUnixTime: undefined, timerId: undefined});
+  const timerValue = myState.startUnixTime ? Math.floor((new Date().getTime()-myState.startUnixTime)/1000 + "с") : undefined;
   return (
     <div className="App">
-      Я приложение.
-      <Component1/>
+      Я приложение. <br/>
+      У меня есть счётчик: {timerValue} с.
+      <Component1 startTimer={()=>setMyState({
+        startUnixTime: new Date().getTime(),
+        currentUnixTime: new Date().getTime(),
+        timerId: setInterval(()=>{setMyState({...myState, currentUnixTime: new Date().getTime()})},1000)})} />
     </div>
   );
 }
--- src/Component1.js
+++ src/Component1.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function Component1() {
+export default function Component1(props) {
   return (
-    <div>
+    <div onClick={props.startTimer}>
       А, я компонент 1.
     </div>
   );
